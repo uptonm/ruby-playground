56c2746c204166d415735e3d467871975653b4a2
Mike Upton <uptonm@wit.edu>
2019-11-18 15:53:37 -0500

    Create input.rb

---
+++ learning-ruby/input.rb
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------------------------------------------------------------------
+# Ruby Input Cheat Sheet
+# Based on a cheat sheet by shortcutFoo @ https://www.shortcutfoo.com/app/dojos/ruby-strings/cheatsheet
+# Formatted by Mike Upton <uptonm>
+#--------------------------------------------------------------------------------------------------------------------------------
+
+puts "What is your first name"
+first_name = gets.chomp
+puts "Thanks #{first_name}"
